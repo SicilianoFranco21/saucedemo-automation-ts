dc6a084d73d04b8d7d2521f2c9ca2326e2378124
Franco S. <Sicilianofranco21@gmail.com>
2026-02-09 17:15:19 -0300

    Several changes on ProductPage and ProductsPages classes

--- pages/product-page.ts
+++ pages/product-page.ts
@@ -1,4 +1,48 @@
+import type { Page, Locator } from "@playwright/test";
 import { BasePage } from "./base-page.js";
+import { HeaderComponent } from "../components/header.component.js";
 
+export class ProductPage extends BasePage {
+  readonly header: HeaderComponent;
+  readonly title: Locator;
+  readonly description: Locator;
+  readonly price: Locator;
+  readonly addToCartButton: Locator;
+  readonly backToProductsButton: Locator;
 
-export class ProductPage {}
+  constructor(page: Page) {
+    super(page);
+    this.header = new HeaderComponent(page);
+    this.title = page.getByTestId("inventory-item-name");
+    this.description = page.getByTestId("inventory-item-desc");
+    this.price = page.getByTestId("inventory_item_price");
+
+    this.addToCartButton = page.locator("button", {
+      hasText: "Add to cart",
+    });
+
+    this.backToProductsButton = page.locator("button", {
+      hasText: "Back to products",
+    });
+  }
+
+  async addToCart(): Promise<void> {
+    await this.addToCartButton.click();
+  }
+
+  async backToProducts(): Promise<void> {
+    await this.backToProductsButton.click();
+  }
+
+  async getTitleText(): Promise<string | null> {
+    return await this.title.textContent();
+  }
+
+  async getDescriptionText(): Promise<string | null> {
+    return await this.description.textContent();
+  }
+
+  async getPriceText(): Promise<string | null> {
+    return await this.price.textContent();
+  }
+}
--- pages/products-page.ts
+++ pages/products-page.ts
@@ -1,20 +1,48 @@
 import type { Page, Locator } from "@playwright/test";
 import { BasePage } from "./base-page.js";
+import { ProductPage } from "./product-page.js";
 import { HeaderComponent } from "../components/header.component.js";
 import { SideMenuComponent } from "../components/side-menu.component.js";
 import { ProductsSortComponent } from "../components/products-sort.component.js";
+import { ProductItemComponent } from "../components/product-item.component.js";
 
 export class ProductsPage extends BasePage {
-    readonly header: HeaderComponent;
-    readonly sideMenu: SideMenuComponent;
-    readonly productsSort: ProductsSortComponent;
-    readonly inventoryTitle: Locator;
+  readonly header: HeaderComponent;
+  readonly sideMenu: SideMenuComponent;
+  readonly productsSort: ProductsSortComponent;
+  readonly products: Locator;
+  readonly inventoryTitle: Locator;
 
-    constructor(page: Page) {
-        super(page);
-        this.header = new HeaderComponent(page);
-        this.sideMenu = new SideMenuComponent(page);
-        this.productsSort = new ProductsSortComponent(page);
-        this.inventoryTitle = page.getByTestId("title");
-    } 
+  constructor(page: Page) {
+    super(page);
+    this.header = new HeaderComponent(page);
+    this.sideMenu = new SideMenuComponent(page);
+    this.productsSort = new ProductsSortComponent(page);
+    this.products = page.getByTestId("inventory-item");
+    this.inventoryTitle = page.getByTestId("title");
+  }
+
+  getProductByName(productName: string): ProductItemComponent {
+    const productRoot: Locator = this.products.filter({
+      has: this.page
+        .getByTestId("inventory-item-name")
+        .filter({ hasText: productName }),
+    });
+    return new ProductItemComponent(productRoot);
+  }
+
+  async getAllProducts(): Promise<ProductItemComponent[]> {
+    const count: number = await this.products.count();
+    const items: ProductItemComponent[] = [];
+    for (let i = 0; i < count; i++) {
+      items.push(new ProductItemComponent(this.products.nth(i)));
+    }
+    return items;
+  }
+
+  async openProductByName(productName: string): Promise<ProductPage> {
+    const product = this.getProductByName(productName);
+    await product.title.click();
+    return new ProductPage(this.page);
+  }
 }
