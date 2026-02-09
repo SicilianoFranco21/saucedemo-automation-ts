814e07b1664d4d475209762cb56736b6f47b2ff0
Franco S. <Sicilianofranco21@gmail.com>
2026-02-09 02:22:01 -0300

    Create ProductsPage class, constructor and attributes

--- pages/products-page.ts
+++ pages/products-page.ts
@@ -1,4 +1,20 @@
+import type { Page, Locator } from "@playwright/test";
 import { BasePage } from "./base-page.js";
+import { HeaderComponent } from "../components/header.component.js";
+import { SideMenuComponent } from "../components/side-menu.component.js";
+import { ProductsSortComponent } from "../components/products-sort.component.js";
 
+export class ProductsPage extends BasePage {
+    readonly header: HeaderComponent;
+    readonly sideMenu: SideMenuComponent;
+    readonly productsSort: ProductsSortComponent;
+    readonly inventoryTitle: Locator;
 
-export class ProductsPage {}
+    constructor(page: Page) {
+        super(page);
+        this.header = new HeaderComponent(page);
+        this.sideMenu = new SideMenuComponent(page);
+        this.productsSort = new ProductsSortComponent(page);
+        this.inventoryTitle = page.getByTestId("title");
+    } 
+}
