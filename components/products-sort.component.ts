eacdac01963ee275dc72fc9a41b09a189f51ecf3
Franco S. <Sicilianofranco21@gmail.com>
2026-02-10 11:41:38 -0300

    Change code format

--- components/products-sort.component.ts
+++ components/products-sort.component.ts
@@ -1,26 +1,25 @@
 import type { Page, Locator } from "@playwright/test";
 
-
 export class ProductsSortComponent {
-    readonly sortDropdown: Locator;
+  readonly sortDropdown: Locator;
 
-    constructor(page: Page) {
-        this.sortDropdown = page.getByTestId("product-sort-container");
-    }
+  constructor(page: Page) {
+    this.sortDropdown = page.getByTestId("product-sort-container");
+  }
 
-    async sortByPriceLowToHigh(): Promise<void> {
-        await this.sortDropdown.selectOption("lohi");
-    }
+  async sortByPriceLowToHigh(): Promise<void> {
+    await this.sortDropdown.selectOption("lohi");
+  }
 
-    async sortByPriceHighToLow(): Promise<void> {
-        await this.sortDropdown.selectOption("hilo");
-    }
+  async sortByPriceHighToLow(): Promise<void> {
+    await this.sortDropdown.selectOption("hilo");
+  }
 
-    async sortByNameAZ(): Promise<void> {
-        await this.sortDropdown.selectOption("az");
-    }
+  async sortByNameAZ(): Promise<void> {
+    await this.sortDropdown.selectOption("az");
+  }
 
-    async sortByNameZA(): Promise<void> {
-        await this.sortDropdown.selectOption("za");
-    }
+  async sortByNameZA(): Promise<void> {
+    await this.sortDropdown.selectOption("za");
+  }
 }
