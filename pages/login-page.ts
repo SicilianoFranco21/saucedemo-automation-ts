abb4a424fe239a70a0d9d9920b336b42a7cfd0d7
Franco S. <Sicilianofranco21@gmail.com>
2026-02-10 11:44:50 -0300

    Change code format

--- pages/login-page.ts
+++ pages/login-page.ts
@@ -1,34 +1,33 @@
 import { BasePage } from "./base-page.js";
 import type { Page, Locator } from "@playwright/test";
 
-
 export class LoginPage extends BasePage {
-    readonly usernameLabel: Locator;
-    readonly passwordLabel: Locator;
-    readonly loginButton: Locator;
+  readonly usernameLabel: Locator;
+  readonly passwordLabel: Locator;
+  readonly loginButton: Locator;
 
-    constructor(page: Page) {
-        super(page);
-        this.usernameLabel = page.getByTestId('username');
-        this.passwordLabel = page.getByTestId('password');
-        this.loginButton = page.getByTestId('login-button');
-    }
+  constructor(page: Page) {
+    super(page);
+    this.usernameLabel = page.getByTestId("username");
+    this.passwordLabel = page.getByTestId("password");
+    this.loginButton = page.getByTestId("login-button");
+  }
 
-    async fillUsernameLabel(username: string): Promise<void> {
-        await this.usernameLabel.fill(username);
-    }
+  async fillUsernameLabel(username: string): Promise<void> {
+    await this.usernameLabel.fill(username);
+  }
 
-    async fillPasswordLabel(password: string): Promise<void> {
-        await this.passwordLabel.fill(password);
-    }
+  async fillPasswordLabel(password: string): Promise<void> {
+    await this.passwordLabel.fill(password);
+  }
 
-    async clickLoginButton(): Promise<void> {
-        await this.loginButton.click();
-    }
+  async clickLoginButton(): Promise<void> {
+    await this.loginButton.click();
+  }
 
-    async login(username: string, password: string): Promise<void> {
-        await this.fillUsernameLabel(username);
-        await this.fillPasswordLabel(password);
-        await this.clickLoginButton();
-    }
+  async login(username: string, password: string): Promise<void> {
+    await this.fillUsernameLabel(username);
+    await this.fillPasswordLabel(password);
+    await this.clickLoginButton();
+  }
 }
