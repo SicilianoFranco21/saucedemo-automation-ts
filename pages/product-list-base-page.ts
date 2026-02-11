fc180ed753c7d90f18f98581a7b1377268765c00
Franco S. <Sicilianofranco21@gmail.com>
2026-02-11 02:19:18 -0300

    Refactor: Add openProduct and getProductByName

--- pages/product-list-base-page.ts
+++ pages/product-list-base-page.ts
@@ -3,6 +3,8 @@ import { BasePage } from "./base-page.js";
 import { HeaderComponent } from "../components/header.component.js";
 import { FooterComponent } from "../components/footer.component.js";
 import { SideMenuComponent } from "../components/side-menu.component.js";
+import { ProductItemComponent } from "../components/product-item.component.js";
+import { ProductPage } from "./product-page.js";
 
 export abstract class ProductListBasePage extends BasePage {
   readonly header: HeaderComponent;
@@ -26,12 +28,23 @@ export abstract class ProductListBasePage extends BasePage {
     });
   }
 
+  getProductByName(name: string): ProductItemComponent {
+    const root = this.getProductRootByName(name);
+    return new ProductItemComponent(root);
+  }
+
   protected getAllProductRoots(): Locator {
     return this.products;
   }
 
-  protected async openProductByName(productName: string): Promise<void> {
-    const productRoot = this.getProductRootByName(productName);
-    await productRoot.getByTestId("inventory-item-name").click();
+  protected async openProductByName(name: string): Promise<void> {
+    await this.getProductRootByName(name)
+      .getByTestId("inventory-item-name")
+      .click();
+  }
+
+  async openProduct(name: string): Promise<ProductPage> {
+    await this.openProductByName(name);
+    return new ProductPage(this.page);
   }
 }
