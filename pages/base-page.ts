5985380fbb4dc169ea3ba60958395219afdbc5cd
Franco S. <Sicilianofranco21@gmail.com>
2026-02-11 02:21:07 -0300

    Create and implement open method

--- pages/base-page.ts
+++ pages/base-page.ts
@@ -1,10 +1,15 @@
 import type { Page } from "@playwright/test";
 
 export abstract class BasePage {
+  protected abstract readonly url: string;
   protected readonly page: Page;
 
   constructor(page: Page) {
     this.page = page;
+  }
+
+  async open(): Promise<void> {
+    await this.page.goto(this.url);
   }
 
   async reloadPage(): Promise<void> {
