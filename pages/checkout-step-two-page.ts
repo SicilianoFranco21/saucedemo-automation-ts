9321efcae94e80b32f266a4e5a5e97ed94dc0477
Franco S. <Sicilianofranco21@gmail.com>
2026-02-11 02:23:06 -0300

    Create class and implement its methods

---
+++ pages/checkout-step-two-page.ts
@@ -0,0 +1,33 @@
+import { FooterComponent } from "../components/footer.component.js";
+import { HeaderComponent } from "../components/header.component.js";
+import { SideMenuComponent } from "../components/side-menu.component.js";
+import type { Page, Locator } from "@playwright/test";
+import { ProductListBasePage } from "./product-list-base-page.js";
+
+export class CheckoutStepTwoPage extends ProductListBasePage {
+  readonly url: string = "/checkout-step-two.html";
+  readonly header: HeaderComponent;
+  readonly footer: FooterComponent;
+  readonly sideMenu: SideMenuComponent;
+  readonly checkoutStepTwoTitle: Locator;
+  readonly cancelButton: Locator;
+  readonly finishButton: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.header = new HeaderComponent(page);
+    this.footer = new FooterComponent(page);
+    this.sideMenu = new SideMenuComponent(page);
+    this.checkoutStepTwoTitle = page.getByTestId("title");
+    this.cancelButton = page.getByTestId("cancel");
+    this.finishButton = page.getByTestId("finish");
+  }
+
+  async cancel(): Promise<void> {
+    await this.cancelButton.click();
+  }
+
+  async finish(): Promise<void> {
+    await this.finishButton.click();
+  }
+}
