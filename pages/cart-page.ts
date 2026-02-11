bde14d76ce146b08a77b6d0eca6517b729085e53
Franco S. <Sicilianofranco21@gmail.com>
2026-02-11 02:18:13 -0300

    Change name convention from PascalCase to camelCase

--- pages/cart-page.ts
+++ pages/cart-page.ts
@@ -1,9 +1,8 @@
 import type { Page, Locator } from "@playwright/test";
 import { ProductListBasePage } from "./product-list-base-page.js";
-import { ProductItemComponent } from "../components/product-item.component.js";
-import { ProductPage } from "./product-page.js";
 
 export class CartPage extends ProductListBasePage {
+  readonly url: string = "/cart.html";
   readonly cartTitle: Locator;
   readonly continueShoppingButton: Locator;
   readonly checkoutButton: Locator;
@@ -15,21 +14,11 @@ export class CartPage extends ProductListBasePage {
     this.checkoutButton = page.getByTestId("checkout");
   }
 
-  getProductByName(productName: string): ProductItemComponent {
-    const root = this.getProductRootByName(productName);
-    return new ProductItemComponent(root);
-  }
-
-  async openProduct(productName: string): Promise<ProductPage> {
-    await super.openProductByName(productName);
-    return new ProductPage(this.page);
-  }
-
   async checkout(): Promise<void> {
     await this.checkoutButton.click();
   }
 
-  async ContinueShopping(): Promise<void> {
+  async continueShopping(): Promise<void> {
     await this.continueShoppingButton.click();
   }
 }
