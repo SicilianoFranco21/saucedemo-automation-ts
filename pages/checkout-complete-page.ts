4afba5ea6cae7c85b2ef200d788cb0f0f6ee6f44
Franco S. <Sicilianofranco21@gmail.com>
2026-02-11 02:26:11 -0300

    Create CheckoutCompletePage and implements methods + attributes

---
+++ pages/checkout-complete-page.ts
@@ -0,0 +1,27 @@
+import { FooterComponent } from "../components/footer.component.js";
+import { HeaderComponent } from "../components/header.component.js";
+import { SideMenuComponent } from "../components/side-menu.component.js";
+import type { Page, Locator } from "@playwright/test";
+import { BasePage } from "./base-page.js";
+
+export class CheckoutCompletePage extends BasePage {
+  readonly url: string = "/checkout-complete.html";
+  readonly header: HeaderComponent;
+  readonly footer: FooterComponent;
+  readonly sideMenu: SideMenuComponent;
+  readonly checkoutStepTwoTitle: Locator;
+  readonly backHomeButton: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.header = new HeaderComponent(page);
+    this.footer = new FooterComponent(page);
+    this.sideMenu = new SideMenuComponent(page);
+    this.checkoutStepTwoTitle = page.getByTestId("title");
+    this.backHomeButton = page.getByTestId("finish");
+  }
+
+  async backHome(): Promise<void> {
+    await this.backHomeButton.click();
+  }
+}
--- pages/checkout-page.ts
+++
@@ -1,8 +0,0 @@
-import { BasePage } from "./base-page.js";
-import type { Page, Locator } from "@playwright/test";
-
-export class CheckoutPage extends BasePage {
-    constructor(page: Page) {
-        super(page);
-    }
-}
