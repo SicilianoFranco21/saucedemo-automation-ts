a64639179a0568c5900c1e361c8512d1350255fd
Franco S. <Sicilianofranco21@gmail.com>
2026-02-11 02:19:54 -0300

    Refactor: Delete overloaded methods

--- pages/products-page.ts
+++ pages/products-page.ts
@@ -1,10 +1,9 @@
 import type { Page, Locator } from "@playwright/test";
 import { ProductListBasePage } from "./product-list-base-page.js";
 import { ProductsSortComponent } from "../components/products-sort.component.js";
-import { ProductItemComponent } from "../components/product-item.component.js";
-import { ProductPage } from "./product-page.js";
 
 export class ProductsPage extends ProductListBasePage {
+  readonly url: string = "/inventory.html";
   readonly productsSort: ProductsSortComponent;
   readonly inventoryTitle: Locator;
 
@@ -12,15 +11,5 @@ export class ProductsPage extends ProductListBasePage {
     super(page);
     this.productsSort = new ProductsSortComponent(page);
     this.inventoryTitle = page.getByTestId("title");
-  }
-
-  getProductByName(productName: string): ProductItemComponent {
-    const root = this.getProductRootByName(productName);
-    return new ProductItemComponent(root);
-  }
-
-  async openProduct(productName: string): Promise<ProductPage> {
-    await super.openProductByName(productName);
-    return new ProductPage(this.page);
   }
 }
