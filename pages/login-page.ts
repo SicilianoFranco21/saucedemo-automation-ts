7ea2a66963d3af4b5c3382855be6e75477e8931e
Franco S. <Sicilianofranco21@gmail.com>
2026-04-14 17:20:09 -0300

    Change double comillas to simple ones. Delete clearPassword and clearUsername methods

--- pages/login-page.ts
+++ pages/login-page.ts
@@ -1,8 +1,9 @@
-import { BasePage } from "./base-page.js";
-import type { Page, Locator } from "@playwright/test";
+import { BasePage } from './base-page.js';
+import type { Page, Locator } from '@playwright/test';
+import type { User } from '../models/user.model.js';
 
 export class LoginPage extends BasePage {
-  readonly url: string = "/";
+  readonly url: string = '/';
   readonly usernameInput: Locator;
   readonly passwordInput: Locator;
   readonly loginButton: Locator;
@@ -10,41 +11,27 @@ export class LoginPage extends BasePage {
 
   constructor(page: Page) {
     super(page);
-    this.usernameInput = page.getByTestId("username");
-    this.passwordInput = page.getByTestId("password");
-    this.loginButton = page.getByTestId("login-button");
-    this.errorMessage = page.getByTestId("error");
-  }
-
-  async navigate(): Promise<void> {
-    await this.page.goto(this.url);
+    this.usernameInput = page.getByTestId('username');
+    this.passwordInput = page.getByTestId('password');
+    this.loginButton = page.getByTestId('login-button');
+    this.errorMessage = page.getByTestId('error');
   }
 
   async fillUsername(username: string): Promise<void> {
     await this.usernameInput.fill(username);
   }
 
-  async clearUsername(): Promise<void> {
-    await this.usernameInput.clear();
-  }
-
   async fillPassword(password: string): Promise<void> {
     await this.passwordInput.fill(password);
-  }
-
-  async clearPassword(): Promise<void> {
-    await this.passwordInput.clear();
   }
 
   async submit(): Promise<void> {
     await this.loginButton.click();
   }
 
-  async login(username: string, password: string): Promise<void> {
-    await this.fillUsername(username);
-    await this.fillPassword(password);
+  async login(user: User): Promise<void> {
+    await this.fillUsername(user.username);
+    await this.fillPassword(user.password);
     await this.submit();
   }
-
-  async loginErrorMessage(): Promise<void> {}
 }
